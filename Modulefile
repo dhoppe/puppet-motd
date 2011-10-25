d7458fad8e0f708c376220b7f99f08e088c24e15
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2011-10-25 21:39:50 +0200

    Initial commit

---
+++ Modulefile
@@ -0,0 +1,9 @@
+name 'dhoppe-motd'
+version '0.0.1'
+
+author 'Dennis Hoppe'
+license 'GPLv2'
+project_page 'http://github.com/dhoppe/puppet-motd'
+source 'git://github.com/dhoppe/puppet-motd'
+summary 'MOTD Module'
+description 'MOTD Module for Debian, Ubuntu'
---
+++ manifests/init.pp
@@ -0,0 +1,42 @@
+class motd {
+	if $lsbdistcodename == "lenny" {
+		file { "/etc/init.d/bootmisc.sh":
+			owner   => root,
+			group   => root,
+			mode    => 0755,
+			source  => "puppet:///modules/motd/$lsbdistcodename/etc/init.d/bootmisc.sh",
+			require => Package["cowsay"],
+		}
+	} elsif $lsbdistcodename == "squeeze" {
+		file { "/etc/init.d/bootlogs":
+			owner   => root,
+			group   => root,
+			mode    => 0755,
+			source  => "puppet:///modules/motd/$lsbdistcodename/etc/init.d/bootlogs",
+			require => Package["cowsay"],
+		}
+	} else {
+		file { "/etc/update-motd.d":
+			recurse => true,
+			owner   => root,
+			group   => root,
+			mode    => 0755,
+			source  => "puppet:///modules/motd/$lsbdistcodename/etc/update-motd.d",
+			require => Package["cowsay"],
+		}
+	}
+
+	file { "/etc/motd.tail":
+		owner   => root,
+		group   => root,
+		mode    => 0644,
+		content => template("motd/common/etc/motd.tail.erb"),
+		require => Package["cowsay"],
+	}
+
+	package { "cowsay":
+		ensure => present,
+	}
+}
+
+# vim: tabstop=3
