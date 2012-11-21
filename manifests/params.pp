f4fe01a4dc7dbc22bc042e22ab23f7aa06e533d1
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-11-21 17:11:56 +0100

    Remove support for Natty    - End of Life on 2012-10-28

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,10 +4,6 @@ class motd::params {
       $file   = '/etc/init.d/bootlogs'
       $source = "puppet:///modules/motd/${::lsbdistcodename}/etc/init.d/bootlogs"
     }
-    'natty': {
-      $file   = '/etc/update-motd.d'
-      $source = "puppet:///modules/motd/${::lsbdistcodename}/etc/update-motd.d"
-    }
     default: {
       fail("Module ${module_name} does not support ${::lsbdistcodename}")
     }
